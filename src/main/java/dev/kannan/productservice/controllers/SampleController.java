3b655aa241a0b69ce2f2a9cdb1bea4271c78099d
kannanthirumal <kannantecht@gmail.com>
2025-07-07 01:40:05 +0530

    Add SampleController with basic greeting endpoints

--- src/main/java/dev/kannan/productservice/controllers/SampleController.java
+++ src/main/java/dev/kannan/productservice/controllers/SampleController.java
@@ -1,4 +1,21 @@
 package dev.kannan.productservice.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/welcome")
 public class SampleController {
+
+    @GetMapping
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    @GetMapping("/{nameOfUser}")
+    public String sayHelloPersonalized(@PathVariable("nameOfUser") String name) {
+        return "Hello " + name + "!";
+    }
 }
